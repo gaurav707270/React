3c2f041d9333b0004057f98e6a3157f7d2ffabbb
gaurav707270 <sachinkharate2004@gmail.com>
2026-02-27 11:50:12 +0530

    27/02/2026

--- day03/src/App.jsx
+++ day03/src/App.jsx
@@ -1,37 +1,15 @@
-// import Counter from "./components/counter"
-// import Products from "./components/products";
 
 
-// const App = () => {
-//   return <>
-//     <Products />
-//   </>
-// }
-
-// export default App; 
-
-// import Counter from "./components/counter"
-// import Products from "./components/Products";
-// import Fourits from "./components/fruits-list"
-
-// const App = () => {
-//   return (
-//     <div>
-//       {/* <Products /> */}
-//       {/* <Counter /> */}
-//       <Fourits />
-//     </div>
-//   );
-// };
-
-// export default App;
-
-import Fruits from "./components/fruits-list";
+import Counter from "./components/counter";
+import Products from "./components/Products";
+import Fourits from "./components/fruits-list";
 
 const App = () => {
   return (
     <div>
-      <Fruits />
+      {/* <Products /> */}
+      <Counter />
+      {/* <Fruits /> */}
     </div>
   );
 };

29b33846e74c85ffc0ac28aa820929c8917e22e4
gaurav707270 <sachinkharate2004@gmail.com>
2026-03-19 15:57:44 +0530

    19/03/2026

---
+++ 07_zeptro/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  )
+}

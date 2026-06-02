fce1345473eefefc5695c7adddcc287be0bf9195
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-06-02 14:50:32 +0530

    02/06/2026

--- todo-app/src/components/UserInput.jsx
+++ todo-app/src/components/UserInput.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useState } from "react";
 
 export default function UserInput() {
+
+    const getTodos = () => {
+        return JSON.parse(localStorage.getItem("todo")) || [];
+    };
+
+    const [inputText, setInputText] = useState("")
+    const [inputPriority, setInputPriority] = useState("")
+    const [todo, setTodo] = useState([])
+    // console.log(todo)
+
     return (
         <div className="container py-5">
             <div
@@ -15,7 +26,9 @@ export default function UserInput() {
                     <div className="row g-3 align-items-center">
                         {/* Task Input */}
                         <div className="col-md-5">
-                            <input
+                            <input onChange={(e) => {
+                                setInputText(e.target.value)
+                            }}
                                 type="text"
                                 className="form-control"
                                 placeholder="Enter your task..."
@@ -24,7 +37,9 @@ export default function UserInput() {
 
                         {/* Priority */}
                         <div className="col-md-4">
-                            <select className="form-select">
+                            <select onChange={(e) => {
+                                setInputPriority(e.target.value)
+                            }} className="form-select">
                                 <option value="">Select Priority</option>
                                 <option value="High">🔴 High</option>
                                 <option value="Normal">🟡 Normal</option>
@@ -34,7 +49,11 @@ export default function UserInput() {
 
                         {/* Button */}
                         <div className="col-md-3">
-                            <button className="btn btn-primary w-100">
+                            <button onClick={() => {
+                                console.log([...todo, { text: inputText, priority: inputPriority }])
+                                setTodo([...todo, { text: inputText, priority: inputPriority }]);
+
+                            }} className="btn btn-primary w-100">
                                 + Add Task
                             </button>
                         </div>
@@ -42,6 +61,6 @@ export default function UserInput() {
                 </div>
             </div>
 
-        </div>
-        )
+        </div >
+    )
 }

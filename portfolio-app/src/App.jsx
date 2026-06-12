cbf525d4ea02a20ffa35101f4eff78fded3584cb
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-06-12 17:43:23 +0530

    12/06/2026

--- portfolio-app/src/App.jsx
+++ portfolio-app/src/App.jsx
@@ -5,12 +5,14 @@ import Home from "./screen/Home";
 import Projects from "./screen/Projects";
 import Experience from "./screen/Experience";
 import Contect from "./screen/Content";
+import Login from './screen/Login';
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Routes>
+        <Route path='/' element={<Login />}></Route>
         <Route path='/home' element={<Home />}></Route>
         <Route path='/projects' element={<Projects />}></Route>
         <Route path='/experience' element={<Experience />}></Route>
---
+++ portfolio-app/src/screen/Login.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+
+export default function Login() {
+    return (
+        <div className=' w-100 d-flex justify-content-center align-items-center'>
+            <form className='shadow p-5 rounded' style={{ width: "400px" }}>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">
+                        Email address
+                    </label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp"
+                    />
+                    <div id="emailHelp" className="form-text">
+                        We'll never share your email with anyone else.
+                    </div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="exampleInputPassword1"
+                    />
+                </div>
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                    <label className="form-check-label" htmlFor="exampleCheck1">
+                        Check me out
+                    </label>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
+            </form>
+
+        </div>
+    )
+}

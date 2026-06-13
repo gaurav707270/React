0c31ae12de59d3daf1ce92dad858f627df6c872b
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-06-13 10:42:09 +0530

    13/06/2026

--- portfolio-app/src/screen/Login.jsx
+++ portfolio-app/src/screen/Login.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from "react-router";
 
+const user = {
+    email: "sachinkharate2004@gmail.com",
+    password: "sachin7072"
+}
 
 export default function Login() {
+    const [newUser, setNewUser] = useState({})
+
+    const navigate = useNavigate()
+
+    const addEmail = (e) => {
+        setNewUser({ ...newUser, email: e.target.value })
+    }
+
+    const addPassword = (e) => {
+        setNewUser({ ...newUser, password: e.target.value })
+    }
+
+    const checkUser = () => {
+        if (user.email == newUser.email && user.password == newUser.password) {
+            navigate("/Home")
+            alert(hjdwwhj)
+        }
+        else if (newUser.email == " ") {
+            alert("Enter the Email")
+        }
+        else if (newUser.password == "") {
+            alert("enter the Password")
+        }
+        else {
+            alert("Enter the correct Email & Password")
+        }
+    }
+
     return (
         <div className=' w-100 d-flex justify-content-center align-items-center'>
             <form className='shadow p-5 rounded' style={{ width: "400px" }}>
@@ -9,7 +42,9 @@ export default function Login() {
                     <label htmlFor="exampleInputEmail1" className="form-label">
                         Email address
                     </label>
+
                     <input
+                        onChange={addEmail}
                         type="email"
                         className="form-control"
                         id="exampleInputEmail1"
@@ -23,21 +58,27 @@ export default function Login() {
                     <label htmlFor="exampleInputPassword1" className="form-label">
                         Password
                     </label>
+
                     <input
+                        onChange={addPassword}
                         type="password"
                         className="form-control"
                         id="exampleInputPassword1"
                     />
                 </div>
                 <div className="mb-3 form-check">
+
                     <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+
                     <label className="form-check-label" htmlFor="exampleCheck1">
                         Check me out
                     </label>
                 </div>
-                <button type="submit" className="btn btn-primary">
+
+                <button onClick={checkUser} type="submit" className="btn btn-primary">
                     Submit
                 </button>
+
             </form>
 
         </div>

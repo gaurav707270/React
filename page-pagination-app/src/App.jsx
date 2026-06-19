eb0a1f868fc613157c40b03e26e9752f098de5bd
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-06-19 18:18:40 +0530

    19/06/2026

---
+++ page-pagination-app/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+
+const url = "https://jsonplaceholder.typicode.com/photos"
+
+export default function App() {
+
+  const [images, setImages] = useState([])
+
+  const handleFetchImage = async () => {
+    const res = await fetch(url)
+    const data = await res.json()
+    setImages(data)
+
+  }
+
+  useEffect(() => {
+    handleFetchImage()
+  }, [])
+
+
+
+  return (
+    <div>
+      {
+        images.map((image, i) => {
+          return <p key={i}>  {image.title}</p>
+        })
+      }
+    </div>
+  )
+}

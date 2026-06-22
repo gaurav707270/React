7fd63a34073e15770b5fc28879de7051fa658392
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-06-22 11:14:28 +0530

    22/06/2026

--- page-pagination-app/src/App.jsx
+++ page-pagination-app/src/App.jsx
@@ -2,56 +2,35 @@ import React, { useState, useEffect } from 'react'
 
 const url = "https://jsonplaceholder.typicode.com/photos"
 
+
 export default function App() {
 
-  const [images, setImages] = useState([])
+  const [allImages, setAllImages] = useState([])
 
-  const handleFetchImage = async () => {
+
+  const fetchdata = async () => {
     const res = await fetch(url)
-    const data = await res.json()
-    setImages(data)
+    const images = await res.json()
+
+    setAllImages(images)
+
   }
 
+  const [page, setPage] = useState({ start: 0, end: 9 })
+
   useEffect(() => {
-    handleFetchImage()
+    fetchdata()
   }, [])
-
-  const [page, setPage] = useState(1)
-
-  const itemsPerPage = 10
-
-  const lastIndex = page * itemsPerPage
-  const firstIndex = lastIndex - itemsPerPage
-
-  const currentImages = images.slice(firstIndex, lastIndex)
-
-  const totalPages = Math.ceil(images.length / itemsPerPage)
 
   return (
     <div>
-      <h2>Images Pagination</h2>
-
-      {currentImages.map((image) => (
-        <p key={image.id}>{image.title}</p>
-      ))}
-
-      <button
-        onClick={() => setPage(page - 1)}
-        disabled={page === 1}
-      >
-        Prev
-      </button>
-
-      <span style={{ margin: "0 10px" }}>
-        Page {page} of {totalPages}
-      </span>
-
-      <button
-        onClick={() => setPage(page + 1)}
-        disabled={page === totalPages}
-      >
-        Next
-      </button>
+      {
+        allImages.map((image, i) =>
+          <div key={i}>
+            <h3>{image.title}</h3>
+          </div>
+        )
+      }
     </div>
   )
 }

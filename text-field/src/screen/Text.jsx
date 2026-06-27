d9953124c48c56f08929ad651aaa38aa506d4b98
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-06-27 13:33:06 +0530

    27/06/2026

--- text-field/src/screen/Text.jsx
+++ text-field/src/screen/Text.jsx
@@ -31,10 +31,38 @@ export default function Text() {
         setUserInfo({ ...userInfo, password: e.target.value })
     }
 
-    const handleSetInfoJson = () => {
-        setTextList([...textList, userInfo])
-        console.log(textList)
+    const handleSetInfoJson = async () => {
+        const res = await axios.post(textListURL, userInfo);
+    }
 
+
+    const handleRemoveUser = async (user) => {
+        const res = await axios.delete(`${textListURL}/${user.id}`)
+        fetchTextList()
+        console.log(user.id)
+    }
+
+    const [isUpdate, setIsUpdate] = useState(false)
+
+    const handleEditUser = (user) => {
+        setUserInfo(user)
+        fetchTextList()
+
+        setIsUpdate(true)
+        console.log(isUpdate)
+        console.log("handle edit")
+    }
+
+    const handleUpdateUser = async () => {
+        const res = await axios.patch(
+            `${textListURL}/${userInfo.id}`,
+            userInfo
+        );
+
+        fetchTextList()
+
+        setIsUpdate(false)
+        console.log("handle update")
     }
 
     return (
@@ -58,6 +86,7 @@ export default function Text() {
             <div className='container d-flex justify-content-evenly my-3  align-items-center'>
                 <div className="form-floating mb-3 w-25">
                     <input
+                        value={userInfo.email}
                         onChange={getUserEmail}
                         type="email"
                         className="form-control shadow"
@@ -69,6 +98,7 @@ export default function Text() {
 
                 <div className="form-floating mb-3 w-25">
                     <input
+                        value={userInfo.role}
                         onChange={getUserRole}
                         type="text"
                         className="form-control shadow"
@@ -80,6 +110,7 @@ export default function Text() {
 
                 <div className="form-floating mb-3 w-25">
                     <input
+                        value={userInfo.password}
                         onChange={getUserPassword}
                         type="password"
                         className="form-control shadow"
@@ -90,10 +121,60 @@ export default function Text() {
                 </div>
 
                 <div className='mt-2 d-flex align-items-center mb-3'>
-                    <button className='btn btn-outline-primary ' onClick={handleSetInfoJson}>Submite</button>
+                    <button
+                        className="btn btn-outline-primary"
+                        onClick={() => {
+                            if (isUpdate) {
+                                handleUpdateUser();
+                                fetchTextList();
+                            } else {
+                                handleSetInfoJson();
+                                fetchTextList();
+                            }
+                        }}
+                    >
+                        {isUpdate ? "Update User" : "Submit"}
+                    </button>
                 </div>
 
             </div>
-        </div>
+
+            {/* diplay user info */}
+            <div className="container mt-4">
+                <div className="row">
+                    {textList.map((user, i) => (
+                        <div className="col-md-6 col-lg-4 mb-4" key={i}>
+                            <div className="card shadow-sm h-100">
+                                <div className="card-body">
+                                    <h5 className="card-title mb-3">{user.email}</h5>
+
+                                    <p className="mb-2">
+                                        <strong>Role :</strong> {user.role}
+                                    </p>
+
+                                    <p className="mb-4">
+                                        <strong>Password :</strong> {user.password}
+                                    </p>
+
+                                    <div className="d-flex justify-content-end gap-2">
+                                        <button
+                                            className="btn btn-outline-primary btn-sm ri-pencil-ai-line"
+                                            onClick={() => handleEditUser(user)}
+                                        >
+                                        </button>
+
+                                        <button onClick={() => {
+                                            handleRemoveUser(user)
+                                        }} className="btn btn-outline-danger ri-delete-bin-5-line btn-sm">
+
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div >
     )
 }

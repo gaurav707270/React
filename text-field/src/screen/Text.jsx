fac9e928ce4fdd3f48840dba6dd4d13c057637bc
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-06-29 13:08:10 +0530

    29/06/2026

--- text-field/src/screen/Text.jsx
+++ text-field/src/screen/Text.jsx
@@ -93,100 +93,142 @@ export default function Text() {
                             height="30"
                             className="rounded me-2"
                         />
-                        Text Field
+                        Text Field Manager
                     </a>
                 </div>
             </nav>
 
             {/* form section */}
-            <div className='container d-flex justify-content-evenly my-3  align-items-center'>
-                <div className="form-floating mb-3 w-25">
-                    <input
-                        value={userInfo.email}
-                        onChange={getUserEmail}
-                        type="email"
-                        className="form-control shadow"
-                        id="floatingInput"
-                        placeholder="name@example.com"
-                    />
-                    <label htmlFor="floatingInput">Email address</label>
-                </div>
+            <div className="container my-5">
+                <div className="card shadow-lg border-0 rounded-4">
+                    <div className="card-header bg-primary text-white text-center py-3">
+                        <h3 className="mb-0">User Information</h3>
+                    </div>
 
-                <div className="form-floating mb-3 w-25">
-                    <input
-                        value={userInfo.role}
-                        onChange={getUserRole}
-                        type="text"
-                        className="form-control shadow"
-                        id="floatingInput"
-                        placeholder="role"
-                    />
-                    <label htmlFor="floatingInput">Job Role</label>
-                </div>
+                    <div className="card-body p-4">
+                        <div className="row g-4 align-items-end">
 
-                <div className="form-floating mb-3 w-25">
-                    <input
-                        value={userInfo.password}
-                        onChange={getUserPassword}
-                        type="password"
-                        className="form-control shadow"
-                        id="floatingPassword"
-                        placeholder="Password"
-                    />
-                    <label htmlFor="floatingPassword">Password</label>
-                </div>
+                            <div className="col-lg-4">
+                                <div className="form-floating">
+                                    <input
+                                        value={userInfo.email}
+                                        onChange={getUserEmail}
+                                        type="email"
+                                        className="form-control"
+                                        id="floatingInput"
+                                        placeholder="Email"
+                                    />
+                                    <label>Email Address</label>
+                                </div>
+                            </div>
 
-                <div className='mt-2 d-flex align-items-center mb-3'>
-                    <button
-                        className="btn btn-outline-primary"
-                        onClick={() => {
-                            if (isUpdate) {
-                                handleUpdateUser();
-                                fetchTextList();
-                                generateRandomColor()
-                            } else {
-                                handleSetInfoJson();
-                                fetchTextList();
-                                generateRandomColor()
-                            }
-                        }}
-                    >
-                        {isUpdate ? "Update User" : "Submit"}
-                    </button>
-                </div>
+                            <div className="col-lg-3">
+                                <div className="form-floating">
+                                    <input
+                                        value={userInfo.role}
+                                        onChange={getUserRole}
+                                        type="text"
+                                        className="form-control"
+                                        id="floatingRole"
+                                        placeholder="Role"
+                                    />
+                                    <label>Job Role</label>
+                                </div>
+                            </div>
 
+                            <div className="col-lg-3">
+                                <div className="form-floating">
+                                    <input
+                                        value={userInfo.password}
+                                        onChange={getUserPassword}
+                                        type="password"
+                                        className="form-control"
+                                        id="floatingPassword"
+                                        placeholder="Password"
+                                    />
+                                    <label>Password</label>
+                                </div>
+                            </div>
+
+                            <div className="col-lg-2 d-grid">
+                                <button
+                                    className={`btn ${isUpdate ? "btn-warning" : "btn-success"} btn-lg`}
+                                    onClick={() => {
+                                        if (isUpdate) {
+                                            handleUpdateUser();
+                                            fetchTextList();
+                                            generateRandomColor();
+                                        } else {
+                                            handleSetInfoJson();
+                                            fetchTextList();
+                                            generateRandomColor();
+                                        }
+                                    }}
+                                >
+                                    {isUpdate ? "Update User" : "Add User"}
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
 
             {/* diplay user info */}
-            <div className="container mt-4">
-                <div className="row">
+            <div className="container my-5">
+                <div className="row g-4">
                     {textList.map((user, i) => (
-                        <div className="col-md-6 col-lg-4 mb-4" key={i} >
-                            <div className="card shadow-sm h-100" style={{ backgroundColor: user.colorCode }} >
-                                <div className="card-body ">
-                                    <h5 className="card-title mb-3">{user.email}</h5>
+                        <div className="col-md-6 col-lg-4" key={i}>
+                            <div
+                                className="card border-0 rounded-4 shadow-lg h-100"
+                                style={{ backgroundColor: user.colorCode }}
+                            >
+                                <div className="card-body p-4">
 
-                                    <p className="mb-2">
-                                        <strong>Role :</strong> {user.role}
+                                    <div className="d-flex justify-content-between align-items-center mb-3 flex-wrap">
+                                        <div className='d-flex flex-wrap'>
+                                            <h5 className="fw-bold text-dark mb-0">
+                                                {user.email}
+                                            </h5>
+                                        </div>
+
+                                        <span className="badge bg-dark">
+                                            {user.role}
+                                        </span>
+                                    </div>
+
+                                    <hr />
+
+                                    <p className="mb-3">
+                                        <strong>Email</strong><br />
+                                        {user.email}
                                     </p>
 
-                                    <p className="mb-4">
-                                        <strong>Password :</strong> {user.password}
+                                    <p className="mb-3">
+                                        <strong>Password</strong><br />
+                                        {user.password}
                                     </p>
 
-                                    <div className="d-flex justify-content-end gap-2">
+                                    <div className="d-flex justify-content-between mt-4">
+
                                         <button
-                                            className="btn btn-outline-primary btn-sm ri-pencil-ai-line"
+                                            className="btn btn-warning rounded-pill px-4"
                                             onClick={() => handleEditUser(user)}
                                         >
+                                            <i className="ri-pencil-line me-1"></i>
+                                            Edit
                                         </button>
 
-                                        <button onClick={() => {
-                                            handleRemoveUser(user)
-                                        }} className="btn btn-outline-danger ri-delete-bin-5-line btn-sm">
-
+                                        <button
+                                            className="btn btn-danger rounded-pill px-4"
+                                            onClick={() => handleRemoveUser(user)}
+                                        >
+                                            <i className="ri-delete-bin-6-line me-1"></i>
+                                            Delete
                                         </button>
+
                                     </div>
+
                                 </div>
                             </div>
                         </div>

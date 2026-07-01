e749beedff2950a5d20208e0cee781490972820f
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-07-01 16:50:38 +0530

    Comment out App function and state initialization

--- 01-day/src/App.jsx
+++ 01-day/src/App.jsx
@@ -3,10 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [count, setCount] = useState(0)
 
-  return (
+//   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">

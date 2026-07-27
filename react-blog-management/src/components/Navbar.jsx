42743d4d22679bf55c2daf252c76d50a1cb08e49
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-07-27 14:31:18 +0530

    27/07/2026

--- react-blog-management/src/components/Navbar.jsx
+++ react-blog-management/src/components/Navbar.jsx
@@ -1,64 +1,66 @@
 import { Link, useLocation } from "react-router";
 
 function Navbar() {
-  const location = useLocation();
+    const location = useLocation();
 
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow">
-      <div className="container">
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow">
+            <div className="container">
 
-        {/* Logo */}
-        <Link className="navbar-brand fw-bold fs-4" to="/">
-          📝 Blog Management
-        </Link>
+                {/* Logo */}
+                <Link className="navbar-brand fw-bold fs-4" to="/">
+                    📝 Blog Management
+                </Link>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-        <div
-          className="collapse navbar-collapse justify-content-end"
-          id="navbarNav"
-        >
-          <ul className="navbar-nav">
+                <div
+                    className="collapse navbar-collapse justify-content-end"
+                    id="navbarNav"
+                >
 
-            <li className="nav-item me-2">
-              <Link
-                to="/"
-                className={`btn ${
-                  location.pathname === "/"
-                    ? "btn-primary"
-                    : "btn-outline-light"
-                }`}
-              >
-                Home
-              </Link>
-            </li>
 
-            <li className="nav-item">
-              <Link
-                to="/add-blog"
-                className={`btn ${
-                  location.pathname === "/add-blog"
-                    ? "btn-success"
-                    : "btn-outline-success"
-                }`}
-              >
-                + Add Blog
-              </Link>
-            </li>
+                    <div className="d-flex mt-3  justify-content-between">
 
-          </ul>
-        </div>
+                        <div className="nav-item me-2">
+                            <Link
+                                to="/"
+                                className={`btn ${location.pathname === "/"
+                                    ? "btn-primary"
+                                    : "btn-outline-light"
+                                    }`}
+                            >
+                                Home
+                            </Link>
+                        </div>
 
-      </div>
-    </nav>
-  );
+                        <div className="nav-item">
+                            <Link
+                                to="/add-blog"
+                                className={`btn ${location.pathname === "/add-blog"
+                                    ? "btn-success"
+                                    : "btn-outline-success"
+                                    }`}
+                            >
+                                + Add Blog
+                            </Link>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </nav>
+    );
+    // 
 }
 
 export default Navbar;

7503e51e19a928309e474f0c540f852375d74b3a
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-07-29 21:48:53 +0530

    Update App.jsx

--- 03_day/src/App.jsx
+++ 03_day/src/App.jsx
@@ -2,7 +2,7 @@
 
 // import Counter from "./components/counter";
 // import Products from "./components/Products";
-import Fourits from "./components/fruits-list";
+// import Fourits from "./components/fruits-list";
 
 const App = () => {
   return (

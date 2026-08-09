acc6e84ea68260a43ff55923fdc07ab2d245b0c1
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-09 13:32:24 +0530

    Import Skills component in App.jsx

--- 05_portfolio/src/App.jsx
+++ 05_portfolio/src/App.jsx
@@ -2,7 +2,7 @@
 // import { Hero } from "./components/02_Hero_section/Hero";
 // import { About } from "./components/03_About_section/About";
 import { Education } from "./components/04_Education_section/Education";
-// import { Skills } from "./components/05_Skills_section/Skills";
+import { Skills } from "./components/05_Skills_section/Skills";
 import { Projects } from "./components/06_Projects_section/Projects";
 import { Contact } from "./components/07_Contact_section/Contact";
 import { Footer } from "./components/08_Footer_section/Footer";

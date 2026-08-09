b763f7abbc971992c40be0fce226b8c4c07fd8ea
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-09 13:32:03 +0530

    Import Projects component in App.jsx

--- 05_portfolio/src/App.jsx
+++ 05_portfolio/src/App.jsx
@@ -3,7 +3,7 @@
 // import { About } from "./components/03_About_section/About";
 import { Education } from "./components/04_Education_section/Education";
 // import { Skills } from "./components/05_Skills_section/Skills";
-// import { Projects } from "./components/06_Projects_section/Projects";
+import { Projects } from "./components/06_Projects_section/Projects";
 import { Contact } from "./components/07_Contact_section/Contact";
 // import { Footer } from "./components/08_Footer_section/Footer";
 

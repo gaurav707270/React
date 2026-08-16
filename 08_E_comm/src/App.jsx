ad7fdb8b728e9e545be6a4d97b72b4d7b3625b89
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-16 10:41:01 +0530

    Update App.jsx

--- 08_E_comm/src/App.jsx
+++ 08_E_comm/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 // import { Route, Routes } from "react-router";
 import Home from './components/home02/Home';
 import Cart from './components/cart03/Cart';
-// import Navbar from './components/navbar01/Navbar';
+import Navbar from './components/navbar01/Navbar';
 // import DetailPage from './components/detail/DetailPage';
 
 

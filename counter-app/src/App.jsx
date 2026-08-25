5f44fdcd4fcc9746522e429b5381b59b3216ce50
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-25 11:14:14 +0530

    Update App.jsx

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -7,7 +7,7 @@
   // return (
     <div className='d-flex justify-content-center align-items-center flex-column vh-100'>
       <h1 className='fs-1 text-white '>{count}</h1>
-      <div>
+      {/* <div> */}
         <button onClick={() => {
           setCount(count + 1)
         }} style={{ width: 150 }} className=' m-2 btn btn-primary'>++</button>

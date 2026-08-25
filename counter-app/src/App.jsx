7376a76d5cda5030b573fde55c7338087b7d3594
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-25 11:16:42 +0530

    Update App.jsx

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -5,7 +5,7 @@ export function App() {
 
   // const [count, setCount] = useState(0);
 
-  // return (
+  return (
     <div className='d-flex justify-content-center align-items-center flex-column vh-100'>
       <h1 className='fs-1 text-white '>{count}</h1>
        <div> 
